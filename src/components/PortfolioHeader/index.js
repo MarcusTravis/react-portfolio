76882dab6a5196ca8468655c4e496ba49224c89f
Marcus <mpt789@gmail.com>
2020-07-12 22:14:39 -0700

    added 90% opacity to porfolio header component

--- src/components/PortfolioHeader/index.js
+++ src/components/PortfolioHeader/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PortfolioHeader() {
     return (
             <div className="row">
-                <header className="col-12 text-center pt-2 pb-1 bg-black text-red">
+                <header className="col-12 text-center pt-2 pb-1 bg-black text-red opacity-90">
                     <p title="I love webdev!" className="h3">
                         Portfolio
                     </p>
