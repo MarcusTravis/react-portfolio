50c40580c4434b8338eeb8b70c2ab8f66fa11672
Marcus <mpt789@gmail.com>
2020-07-13 12:34:42 -0700

    Updates

---
+++ precache-manifest.9959581bbd6d5c8acdd356fce973dc3c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c96c56bbf94ac546b380df32a37e89",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d49174c67398b2419e2d",
+    "url": "/react-portfolio/static/css/main.ce1accce.chunk.css"
+  },
+  {
+    "revision": "73d8ba393e59e102910b",
+    "url": "/react-portfolio/static/js/2.ab373755.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.ab373755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d49174c67398b2419e2d",
+    "url": "/react-portfolio/static/js/main.7690c365.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "8d285cf7d86ea13e6a1926a516516081",
+    "url": "/react-portfolio/static/media/BudgetTracker.8d285cf7.PNG"
+  },
+  {
+    "revision": "12e33c35f667e68e02f479dce5df899d",
+    "url": "/react-portfolio/static/media/Employee-Tracker.12e33c35.PNG"
+  },
+  {
+    "revision": "5e933b8390018600c8943297345b0ac5",
+    "url": "/react-portfolio/static/media/EmployeeDirectory.5e933b83.PNG"
+  },
+  {
+    "revision": "2ad45c6a6b702fb5cc95a74e6aae4e1c",
+    "url": "/react-portfolio/static/media/Fitness.2ad45c6a.PNG"
+  },
+  {
+    "revision": "9a8afec0feb095f9f71f45990a233642",
+    "url": "/react-portfolio/static/media/LogoBlck.9a8afec0.png"
+  },
+  {
+    "revision": "8ecb989a0b7abc77616a39a69f440ba4",
+    "url": "/react-portfolio/static/media/ProfilePic.8ecb989a.png"
+  },
+  {
+    "revision": "da2c6f6e34742d417ac6d4d6f15337f8",
+    "url": "/react-portfolio/static/media/Template.da2c6f6e.PNG"
+  },
+  {
+    "revision": "2443f8f822c186b8f1319c534bc3c388",
+    "url": "/react-portfolio/static/media/mT1red.2443f8f8.png"
+  },
+  {
+    "revision": "f4d9e84b510f132f4cdc9d6515ec8195",
+    "url": "/react-portfolio/static/media/star-wars-backgrounds-26.f4d9e84b.png"
+  },
+  {
+    "revision": "d5a5382efdf3d74558b28f071d788448",
+    "url": "/react-portfolio/static/media/weatherDash.d5a5382e.PNG"
+  }
+]);
